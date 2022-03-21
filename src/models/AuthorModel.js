56a4289ba7b586c017600af38459eb6ebc032e2b
Sonu0911 <98635628+Sonu0911@users.noreply.github.com>
2022-03-21 13:28:58 +0530

    Update AuthorModel.js

--- src/models/AuthorModel.js
+++ src/models/AuthorModel.js
@@ -1,31 +1,39 @@
-const mongoose = require("mongoose")
-const AuthorModel = mongoose.Schema({
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const authorSchema = new mongoose.Schema({
     fname: {
         type: String,
-        required: true
+        required: true,
     },
     lname: {
         type: String,
-        required: true
+        required: true,
     },
-
     title: {
         type: String,
         enum: ["Mr", "Mrs", "Miss"],
-        required: true
+        required: true,
     },
-
     email: {
         type: String,
         lowercase: true,
+        trim: true,
+        unique: true,
         required: true,
-        unique: true
+        validate: {
+            validator: validator.isEmail,
+            message: "{VALUE} is not a valid email",
+            isAsync: false,
+        },
     },
-
     password: {
         type: String,
+        unique: true,
+        trim: true,
         required: true
-    }
 
-}, { timestamps: true })
-module.exports = mongoose.model("authorsproject1", AuthorModel)
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('authorsproject1', authorSchema)
