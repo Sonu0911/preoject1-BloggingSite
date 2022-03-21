e4b024983b9e5ed8e94840e33216a1c47891acfa
Sonu0911 <98635628+Sonu0911@users.noreply.github.com>
2022-03-21 13:23:39 +0530

    Update middleware.js

--- src/controllers/middleware.js
+++ src/controllers/middleware.js
@@ -1,27 +1,44 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
+const BlogModel = require("../models/blogsModel")
+const authenticate = function(req, res, next) {
 
-const middleware = async (req, res, next) => {
+    let token = req.headers["x-api-key"]
+    if (!token) return res.send({ status: false, msg: "token must be present in the request header" })
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+    if (!decodedToken) return res.send({ status: false, msg: "token is not valid" })
+
+    next()
+};
+
+const authorisation = async function(req, res, next) {
     try {
-        const token = req.header('x-api-key')
-        if (!token) {
-            res.status(403).send({ status: false, message: ` token request missing here` })
-            return;
+        let token = req.headers["x-api-key"];
+        let decodedtoken = jwt.verify(token, "functionup-thorium")
+
+        let toBeupdatedblogId = req.params.blogId
+        if (toBeupdatedblogId) {
+            let updatingAuthorId = await BlogModel.find({ _id: toBeupdatedblogId }).select({ authorId: 1, _id: 0 })
+            let authorId = updatingAuthorId.map(x => x.authorId)
+            console.log(authorId)
+            let id = decodedtoken.AuthorId
+            if (id != authorId) return res.status(403).send({ status: false, msg: "You are not authorised to perform this task" })
+        } else {
+            let AuthorId = req.query.authorId
+            toBeupdatedblogId = AuthorId
+            let id = decodedtoken.AuthorId
+            if (id != AuthorId) return res.status(403).send({ status: false, msg: "You are not authorised to perform this task" })
         }
-
-        const decodedtoken = await jwt.verify(token, 'functionup-thorium')
-
-        if (!decodedtoken) {
-            res.status(403).send({ status: false, message: `invalid authenticated token in request body` })
-            return;
-        }
-
-        req.authorId = decodedtoken.authorId;
-
         next()
-    } catch (err) {
-        console.error(`error ${err.message}`)
-        res.status(500).send({ status: false, message: err.message })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: error.message })
     }
 }
 
-module.exports = middleware
+
+
+
+
+module.exports.authenticate = authenticate
+module.exports.authorisation = authorisation
