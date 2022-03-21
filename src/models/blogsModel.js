b7f3b71938ceef6a495ca3fc45f6b69ae45f16ea
Sonu0911 <98635628+Sonu0911@users.noreply.github.com>
2022-03-21 13:29:39 +0530

    Update blogsModel.js

--- src/models/blogsModel.js
+++ src/models/blogsModel.js
@@ -1,47 +1,36 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
-const blogsSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const blogSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
     },
-
     body: {
         type: String,
         required: true,
     },
-
     authorId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "authorsproject1"
+        type: ObjectId,
+        ref: "authorsproject1",
     },
-    tags: ["String"],
-
+    tags: [String],
     category: {
         type: String,
-        required: true
+        required: true,
     },
-    subcategory: ["String"],
-
-    isPublished: {
-        type: Boolean,
-        default: false
-    },
-
-    publishedAt: {
-        type: Date,
-        default: null
-    },
-
+    subcategory: [String],
     isDeleted: {
         type: Boolean,
         default: false
     },
-    deletedAt: {
-        type: Date
+    publishedAt: Date,
+    isPublished: {
+        type: Boolean,
+        default: false,
     },
-
+    deletedAt: Date
 }, { timestamps: true });
-module.exports = mongoose.model('Blog', blogsSchema)
+
+
+module.exports = mongoose.model("Blog", blogSchema);
